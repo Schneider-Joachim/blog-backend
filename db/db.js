2dbc3265d8eefb75567330d332b7c1d26041e7dc
Schneider-Joachim <92768254+Schneider-Joachim@users.noreply.github.com>
2023-02-22 18:52:30 -0500

    pushing

---
+++ db/db.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+import PostModel from "./Post.js";
+import UserModel from "./User.js";
+
+const db = new Sequelize("postgres://worktime@localhost:5432/blog");
+const Post = PostModel(db);
+const User = UserModel(db);
+
+const connectToDB = async () => {
+	try {
+		await db.authenticate();
+		console.log("Connected to DB successfully");
+
+		db.sync();
+	} catch (error) {
+		console.error(error);
+		console.error("PANIC! DB POBLEM!");
+	}
+};
+
+connectToDB();
+
+export { db, Post, User };
+
---
+++ server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import { db, Post, User } from "./db/db.js";
+import bcrypt from "bcrypt";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+	res.send("Server on");
+}); 
+
+server.post("/newPost", async (req, res)=>{
+    await Post.create(req.body);
+    res.send({status: "ok"});
+});
+
+server.get("/posts", async (req,res) => {
+    const posts = await Post.findAll();
+    res.send({ posts })
+});
+
+server.get("/post/:id", async (req, res) => {
+	const post = await Post.findByPk(req.params.id);
+	res.send({ post });
+});
+
+server.delete("/post/:id", async (req, res) => {
+	const post = await Post.findByPk(req.params.id);
+	await post.destroy();
+	res.send({ deleted: "DELETED"});
+});
+
+
+server.post("/login", async (req, res) => {
+	res.send({ loggedIn: true });
+});
+
+const serverStarted = async () => { 
+	const user = await User.findOne({ email: "sj@gmail.com" });
+	if (!user) {
+		await User.create({
+			email:"sj@gmail.com",
+			firstName: "Snites",
+			password: bcrypt.hashSync("Okurr", 10),
+		});
+	}
+};
+serverStarted();
+
+server.listen(3001, () => {
+	console.log("Yo! Server is running on port 3001!");
+});
