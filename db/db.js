43855297d36a6f1de0e05b9fbc2601e94fe88d0e
Schneider-Joachim <92768254+Schneider-Joachim@users.noreply.github.com>
2023-03-01 18:08:55 -0500

    arch

--- db/db.js
+++ db/db.js
@@ -2,7 +2,23 @@ import Sequelize from "sequelize";
 import PostModel from "./Post.js";
 import UserModel from "./User.js";
 
-const db = new Sequelize("postgres://worktime@localhost:5432/blog");
+
+let db; 
+if (process.env.RDS_HOSTNAME) {
+	console.log("Connecting to RDS", process.env.RDS_HOSTNAME);
+	// if we're running on elasticbeanstalk, use elasticbeanstalk connection
+	db = new Sequelize(`postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`, {
+		logging: false,
+	})
+} else {
+	console.log("Connecting to local database");
+	// if we're running locally, use the localhost connection
+	db = new Sequelize("postgres://hackupstate@localhost:5432/blog", {
+		logging: false,
+	});
+}
+
+// const db = new Sequelize("postgres://worktime@localhost:5432/blog");
 const Post = PostModel(db);
 const User = UserModel(db);
 
